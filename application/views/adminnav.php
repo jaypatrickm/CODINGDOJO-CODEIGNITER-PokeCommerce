084daa380f4885c67ed2ccc3b642e12cfed56fe0
Stanley <sliang07@yahoo.com>
2015-05-04 14:48:41 -0700

    checkout and admin some stuff

---
+++ application/views/adminnav.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+	<title></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<script type="text/javascript" src="/assets/jQuery-1.11.2.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+		<style type="text/css">
+			.container-fluid{
+				background-color: #800000;
+			}
+		</style>
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+				    <div class="container-fluid">
+
+				    <!-- Brand and toggle get grouped for better mobile display -->
+					    <div class="navbar-header">
+					      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					        <span class="sr-only">Toggle navigation</span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					      </button>
+					      <a class="navbar-brand" href="#">Dashboard</a>
+					      <a class="navbar-brand" href="#">Orders</a>
+					      <a class="navbar-brand" href="#">Products</a>
+					    </div>
+
+				    <!-- Collect the nav links, forms, and other content for toggling -->
+				    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			      	<ul class="nav navbar-nav navbar-right">
+			        	<li><a href="#">Logout</a></li>
+			    	</ul>
+				    </div><!-- /.navbar-collapse -->
+				  </div><!-- /.container-fluid -->
+				</nav>
+</body>
+</html>
