10c2b1b6ad249444f5dcd5eb9406f316cfee0a31
Jay Manalansan <jaypatrickm@gmail.com>
2015-05-05 11:35:09 -0700

    trial

---
+++ application/controllers/Mains.php
@@ -0,0 +1,64 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Mains extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->output->enable_profiler();
+		$this->load->model('main');
+	}
+
+	public function index()
+	{
+		// $this->load->view('admin_orders');
+		//$this->load->view('product_listing');
+		if($this->session->userdata('carttotal'))
+		{
+			$this->session->userdata('carttotal');
+		}
+		else
+		{
+			$this->session->set_userdata('carttotal',0);
+		}
+		// $this->load->view('admin_orders');
+		//$this->load->view('product_listing');
+			$frontproductbyprice = $this->main->loadfrontproductsbyprice();
+			$this->load->view('products/product_listing',array(
+				'frontproductbyprice' => $frontproductbyprice)
+			);
+		$this->load->view('products/product_listing');
+	}
+
+	public function admin_login(){
+		$this->load->view('admins/admin_login');
+	}
+
+	public function show_product(){
+		$this->load->view('products/show');
+	}
+
+	public function checkout(){
+		$cartitems = $this->main->get_cart();
+		$this->load->view('products/checkout',array(
+			'cartitems' =>$cartitems)
+		);
+		$this->load->view('index');
+	}
+
+	public function admin_orders(){
+		$this->load->view('admins/admin_orders');
+	}
+
+	public function admin_edit_product(){
+		$this->load->view('admins/admin_edit_product');
+	}
+		public function buytocart(){
+
+		$quantity =  $this->input->post('quantity');
+		$this->session->set_userdata('carttotal', $this->session->userdata('carttotal') + $quantity);
+		redirect('product');
+	}
+}
+
+//end of main controller
